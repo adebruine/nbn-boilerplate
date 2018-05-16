20adc60ee72933a0854164a94255da33e8bd7d15
Audrey DeBruine <audrey.debruine@gmail.com>
2018-05-16 12:26:56 -0500

    reads in external hbs templategit add .

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 var _ = require('lodash');
 //import './css/style.css';
-
-function generateBody() {
-  console.log("yo")
-}
-
+var Handlebars = require('handlebars');
+var fs = require('fs');
+//var prettifyHtml = require('prettify-html');
+/*
 function component() {
   var element = document.createElement('div');
 
@@ -12,7 +11,38 @@ function component() {
 
   return element;
 }
+*/
 
-document.body.appendChild(component());
+var pageTemplate = require("./templates/article.hbs");
+/*
+var pageTemplate =
+  "<div class='entry'>"+
+  "<h1>{{title}}</h1>"+
+  "<div class='body'>"+
+    "{{body}}"+
+  "</div>"+
+"</div>";
+*/
 
-generateBody()
+function build() {
+  var context = {title: "My New Post", body: "This is my first post!", slug: "hello"};
+
+    //create the directory if not already created
+		var dir = './../dist/articles/'+context.slug; //if we change how this file is being run, this might bug out
+    if (!fs.existsSync(dir)) {
+      fs.mkdirSync(dir);
+		};
+
+    // write file from template
+    var fileName = './../dist/articles/'+context.slug+'/index.html';
+    var stream = fs.createWriteStream(fileName);
+    var articleResult = pageTemplate(context);
+    //var prettifiedResult = prettifyHtml(articleResult);
+    stream.write(articleResult);
+    stream.end();
+}
+
+//document.body.appendChild(component());
+
+
+build()
