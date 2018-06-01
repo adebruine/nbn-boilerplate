db49d83cfa76f0b02af4696949b47b7c7d2ec678
Audrey DeBruine <audrey.debruine@gmail.com>
2018-05-31 19:16:58 -0500

    almost done

--- src/index.js
+++ src/index.js
@@ -2,37 +2,32 @@ var _ = require('lodash');
 //import './css/style.css';
 var Handlebars = require('handlebars');
 var fs = require('fs');
-//var prettifyHtml = require('prettify-html');
-/*
-function component() {
-  var element = document.createElement('div');
-
-  //element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
-  return element;
-}
-*/
+var mkdirp = require('mkdirp');
+var prettifyHtml = require('prettify-html');
 
 var pageTemplate = require("./templates/article.hbs");
+var context = require("./stories.json");
 
 function build() {
-  var context = [{title: "My New Post", body: "This is my first post!", slug: "hello"},
-                 {title: "My New Post 2", body: "This is my second post!", slug: "hello2"}];
 
-  for (var i = 0; i < context.length; i++) {
+  for (var i = 0; i < context.stories.length; i++) {
     //create the directory if not already created
-		var dir = './../dist/articles/'+context[i].slug; //if we change how this file is being run, this might bug out
-    if (!fs.existsSync(dir)) {
-      fs.mkdirSync(dir);
+    var dir = './../dist/articles/'+context.stories[i].slug; //if we change how this file is being run, this might bug out
+
+
+    if (!fs.existsSync('./../dist/articles/'+context.stories[i].slug)) {
+      fs.mkdirSync('./../dist/articles/'+context.stories[i].slug);
 		};
 
     // write file from template
-    var fileName = './../dist/articles/'+context[i].slug+'/index.html';
+    var fileName = './../dist/articles/'+context.stories[i].slug+'/index.html';
     var stream = fs.createWriteStream(fileName);
-    var articleResult = pageTemplate(context[i]);
+    var articleResult = pageTemplate(context.stories[i]);
+    var fixedResult = articleResult.replace("&lt;p&gt;", "<p>");
     //var prettifiedResult = prettifyHtml(articleResult);
     stream.write(articleResult);
     stream.end();
+
   }
 }
 
